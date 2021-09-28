da1d9ad07f4f4294cdfba2e9a0b94ecb75cfd3a6
JoshuCR <86824705+JoshuCR@users.noreply.github.com>
2021-09-27 21:40:08 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,5 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function LED1_On() {
-	//alert("led on");
-	console.log("led on");
-	//document.getElementById("sensor").innerHTML="led on";
-	message = new Paho.MQTT.Message("ON");
-    	message.destinationName = "jicordova.fie@unach.edu.ec/test1";
-    	client.send(message);
-  
-}
-function LED1_Off(){	
-	//alert("led off");
-	console.log("led off");
-	message = new Paho.MQTT.Message("OFF");
-    	message.destinationName = "jicordova.fie@unach.edu.ec/test1";
-    	client.send(message);
-	//document.getElementById("sensor").innerHTML="led off";
-}
-
 
 
 
@@ -71,6 +53,24 @@ function LED1_Off(){
 	  document.getElementById("sensor").innerHTML=message.payloadString;
   }
 
+
+function LED1_On() {
+	//alert("led on");
+	console.log("led on");
+	//document.getElementById("sensor").innerHTML="led on";
+	message = new Paho.MQTT.Message("ON");
+    	message.destinationName = "jicordova.fie@unach.edu.ec/test1";
+    	client.send(message);
+  
+}
+function LED1_Off(){	
+	//alert("led off");
+	console.log("led off");
+	message = new Paho.MQTT.Message("OFF");
+    	message.destinationName = "jicordova.fie@unach.edu.ec/test1";
+    	client.send(message);
+	//document.getElementById("sensor").innerHTML="led off";
+}
 
 
 
