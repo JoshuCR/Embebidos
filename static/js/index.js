ae65338e80aaf4108107c641cc4f4792eed5dbaa
JoshuCR <86824705+JoshuCR@users.noreply.github.com>
2021-09-27 22:05:56 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -50,7 +50,10 @@ function LED1_Off(){
     message.destinationName = "jicordova.fie@unach.edu.ec/test";
     client.send(message);
     
-  
+  client.subscribe("jicordova.fie@unach.edu.ec/test3");
+    message3 = new Paho.MQTT.Message("hola desde la web");
+    message3.destinationName = "jicordova.fie@unach.edu.ec/test3";
+    client.send(message3);
   }
   function doFail(e){
     console.log(e);
@@ -70,9 +73,9 @@ function LED1_Off(){
 	  document.getElementById("sensor").innerHTML=message.payloadString;
   }
  // called when a message arrives
-  function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor2").innerHTML=message.payloadString;
+  function onMessageArrived(message3) {
+    console.log("onMessageArrived:"+message3.payloadString);
+	  document.getElementById("sensor2").innerHTML=message3.payloadString;
   }
 
 
