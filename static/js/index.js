f3b97af303edf64e24591603f8f59513c5d6c0c0
JoshuCR <86824705+JoshuCR@users.noreply.github.com>
2021-09-27 21:14:19 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -103,7 +103,7 @@ function LED1_Off(){
     console.log("Conectado...");
 	
     client.subscribe("jicordova.fie@unach.edu.ec/test3");
-    message = new Paho.MQTT.Message("hola desde la web");
+    message = new Paho.MQTT.Message("hola des");
     message.destinationName = "jicordova.fie@unach.edu.ec/test3";
     client.send(message);
 	
