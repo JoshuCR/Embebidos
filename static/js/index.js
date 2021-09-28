f402b372a94a914a08e227102dd96694abfe34a7
JoshuCR <86824705+JoshuCR@users.noreply.github.com>
2021-09-27 22:03:12 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -72,7 +72,7 @@ function LED1_Off(){
  // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor2).innerHTML=message.payloadString;
+	  document.getElementById("sensor2").innerHTML=message.payloadString;
   }
 
 
