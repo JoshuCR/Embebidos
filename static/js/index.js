5c104023a61ec8a01cee064324fb1d9ad400f16b
JoshuCR <86824705+JoshuCR@users.noreply.github.com>
2021-09-27 21:22:09 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-//https://www.eclipse.org/paho/clients/js/
+x//https://www.eclipse.org/paho/clients/js/
 
 function LED1_On() {
 	//alert("led on");
@@ -72,58 +72,5 @@ function LED1_Off(){
   }
 
 
-
-
-
-
-
-
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
-  client2 = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
-  // set callback handlers
-  client2.onConnectionLost = onConnectionLost;
-  client2.onMessageArrived = onMessageArrived;
-  var options = {
-   useSSL: false,
-    userName: "jicordova.fie@unach.edu.ec",
-    password: "ironflight666",
-    onSuccess:onConnect,
-    onFailure:doFail
-  }
-
-  // connect the client
-  client2.connect(options);
-   
-  // called when the client connects
-  function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado...");
-	
-    client.subscribe("jicordova.fie@unach.edu.ec/test3");
-    messagex = new Paho.MQTT.Message("hola des");
-    messagex.destinationName = "jicordova.fie@unach.edu.ec/test3";
-    client.send(messagex);
-	
-  }
-  function doFail(e){
-    console.log(e);
-	
-  }
-
-  // called when the client loses its connection
-  function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
-    }
-  }
-
-  // called when a message arrives
-  function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor2").innerHTML=message.payloadString;
-  }
 
 
